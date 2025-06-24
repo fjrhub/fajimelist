7a9cdfa4d75fd1b8d96d976d616f835d838ec86a
fjrhub <muhammadfajrulfalah@proton.me>
2025-06-24 11:15:31 +0700

    feat(CollectionButton): implement onClick handler to fetch and send anime data

--- src/components/AnimeList/CollectionButton.jsx
+++ src/components/AnimeList/CollectionButton.jsx
@@ -2,17 +2,17 @@
 
 import React, { useState } from 'react'
 
-const CollectionButton = ({ anime_mal_id, user_email }) => {
+const CollectionButton = ({ anime_mal_id, user_email, anime_title, anime_image }) => {
   const [isCreated, setIsCreated] = useState(false)
 
   const handleCollection = async (event) => {
     event.preventDefault()
-    const data = { anime_mal_id, user_email }
+    const data = { anime_mal_id, user_email, anime_title, anime_image }
 
     const response = await fetch('/api/v1/collection', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json', // penting!
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
     })
