bdd9889c146fad753ab9519f2dd0276fcfca6eeb
fjrhub <muhammadfajrulfalah@proton.me>
2025-06-24 11:14:49 +0700

    feat(ui): show anime_image from database in collection list

--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -14,19 +14,20 @@ const Page = async () => {
   return (
     <section className="mt-4 px-4 w-full">
       <Header title={'My Collection'} />
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid_cols-4">
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
         {data.map((data, index) => {
+            console.log(data.anime_image)
           return (
-            <Link key={index} href={`/anime/${data.anime_mal_id}`} className="relative border-2 border-color-accent">
+            <Link key={index} href={`/anime/${data.anime_mal_id}`} className="relative">
               <Image
-                scr=""
-                alt=""
+                src={data.anime_image}
+                alt={data.anime_image}
                 width={350}
                 height={350}
                 className="w-full"
               />
               <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-                <h5 className="text-xl text-center">{data.anime_mal_id}</h5>
+                <h5 className="text-xl text-center">{data.anime_title}</h5>
               </div>
             </Link>
           )
